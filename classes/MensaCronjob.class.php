a28d27109b58fce8a70ef54b6402c0052552105d
David Siegfried <ds.siegfried@gmail.com>
2017-03-28 14:58:37 +0200

    add english-lang and fix cronjob, fixes #1

--- classes/MensaCronjob.class.php
+++ classes/MensaCronjob.class.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Class MensaCrojob
  *
@@ -9,48 +10,48 @@
  */
 class MensaCronjob extends CronJob
 {
-    private static $curl_timeout = 40;
-    
+    private static $curl_timeout = 50;
+
     public static function getName()
     {
         return _('Mensa-Cronjob');
     }
-    
+
     public static function getDescription()
     {
         return _('Lädt die Speisepläne für das Studentenwerk Osnabrück herunter');
     }
-    
+
     public function setUp()
     {
         \PluginEngine::getPlugin('MensaPlugin');
     }
-    
+
     public function execute($last_result, $parameters = [])
     {
         $sourceFile = 'ftp://' . Config::get()->MENSA_FTP_SERVER . '/' . Config::get()->MENSA_FTP_FILE;
-        $filename   = MensaHelper::getFilename();
+        $filename   = \MensaHelper::getFilename();
         $curl       = curl_init();
-        $file       = fopen($filename, 'w');
-        
+
         curl_setopt($curl, CURLOPT_URL, $sourceFile);
         curl_setopt($curl, CURLOPT_USERPWD, Config::get()->MENSA_FTP_USER . ':' . Config::get()->MENSA_FTP_PASS);
-        curl_setopt($curl, CURLOPT_FAILONERROR, 1);
-        curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($curl, CURLOPT_TIMEOUT, self::$curl_timeout);
-        curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
-        curl_setopt($curl, CURLOPT_FILE, $file);
-        
-        if (curl_exec($curl)) {
+
+        $result = curl_exec($curl);
+
+        if ($result) {
             echo "Der Import der Mensa-Datei war nicht erfolgreich!\n";
         } else {
             echo "Der Import der Mensa-Datei war erfolgreich\n";
         }
-        
+
         curl_close($curl);
-        fclose($file);
-        
+
+        if ($result !== false) {
+            file_put_contents($filename, $result);
+        }
+
         if (!isset($_SERVER)) {
             chown($filename, 'www-data');
             chgrp($filename, 'www-data');
--- classes/MensaHelper.class.php
+++ classes/MensaHelper.class.php
@@ -10,51 +10,59 @@
  */
 class MensaHelper
 {
-    public static function getMenu($timstamp = null)
+    public static function getMenu($timestamp = null)
     {
         $file = self::getFilename();
-        
+
         if (!file_exists($file)) {
             return [];
         }
-        $handler = fopen($file, 'r');
-        $data    = [];
+        $handler  = fopen($file, 'r');
+        $data     = [];
+        $language = substr($_SESSION['_language'], 0, 2);
         if ($handler !== false) {
             $headLine = fgetcsv($handler, 0, '	');
+            $pos      = array_change_key_case(array_flip($headLine));
             while (($row = fgetcsv($handler, 0, '	')) !== false) {
-                $order[] = $row[5];
-                if ($row[0] == Config::get()->MENSA_LOCATION) {
-                    $data[strtotime($row[1])][$row[5]][$row[4]][$headLine[6]]  = $row[6];
-                    $data[strtotime($row[1])][$row[5]][$row[4]][$headLine[7]]  = $row[7];
-                    $data[strtotime($row[1])][$row[5]][$row[4]][$headLine[8]]  = $row[8];
-                    $data[strtotime($row[1])][$row[5]][$row[4]][$headLine[10]] = $row[10];
-                    $data[strtotime($row[1])][$row[5]][$row[4]][$headLine[11]] = number_format(str_replace(',', '.', $row[11]), 2, ',', '.');
-                    $data[strtotime($row[1])][$row[5]][$row[4]][$headLine[12]] = number_format(str_replace(',', '.', $row[12]), 2, ',', '.');
-                    $data[strtotime($row[1])][$row[5]][$row[4]][$headLine[13]] = number_format(str_replace(',', '.', $row[13]), 2, ',', '.');
-                    $data[strtotime($row[1])][$row[5]][$row[4]][$headLine[14]] = $row[14];
-                    $data[strtotime($row[1])][$row[5]][$row[4]][$headLine[15]] = $row[15];
-                    $data[strtotime($row[1])][$row[5]][$row[4]][$headLine[16]] = $row[16];
+                if ($row[$pos['mensa']] == Config::get()->MENSA_LOCATION) {
+                    $date  = strtotime($row[$pos['datum']]);
+                    $order = $row[$pos['speise_bezeichnung']];
+                    $item  = [
+                        'TEXT1'       => $row[$pos['text1']],
+                        'TEXT2'       => $row[$pos['text2']],
+                        'TEXT3'       => $row[$pos['text3']],
+                        'STD_PREIS'   => number_format(str_replace(',', '.', $row[$pos['std_preis']]), 2, ',', '.'),
+                        'BED_PREIS'   => number_format(str_replace(',', '.', $row[$pos['bed_preis']]), 2, ',', '.'),
+                        'GÄSTE_PREIS' => number_format(str_replace(',', '.', $row[$pos['gäste_preis']]), 2, ',', '.'),
+                        'FREI1'       => $row[$pos['frei1']],
+                        'ZSNUMMERN'   => $row[$pos['zsnummern']],
+                        'ZSNAMEN'     => $row[$pos['zsnamen']],
+                    ];
+                    if ($language == 'en') {
+                        $item['TEXT1'] = $row[$pos['text1_1']] ?: $item['TEXT1'];
+                        $item['TEXT2'] = $row[$pos['text2_1']] ?: $item['TEXT2'];
+                        $item['TEXT3'] = $row[$pos['text3_1']] ?: $item['TEXT3'];
+                    }
+                    $data[$date][$order][] = $item;
                 }
             }
-            fclose($handler);
+            fclose($handler);;
         }
-        if (!$timstamp) {
-            return $data;
-        }
-        
-        return $data[$timstamp];
+        return $timestamp ? $data[$timestamp] : $data;
     }
-    
+
+
+
     public static function getFilename()
     {
         return $GLOBALS['TMP_PATH'] . '/mensa.txt';
     }
-    
+
     public static function replace($string)
     {
         $patterns     = ['/\(/', '/\)/'];
         $replacements = ['<sup>', '</sup>'];
-        
+
         return preg_replace($patterns, $replacements, htmlReady($string));
     }
 }
